a521d5dc501304213ab4d1dd4eb397514aeac682
JaehongDev <println@kakao.com>
2023-04-04 22:27:36 +0900

    setup development containerless

---
+++ jaehong/helloboot/src/main/java/com/example/helloboot/HellobootApplication.java
@@ -0,0 +1,9 @@
+package com.example.helloboot;
+
+
+public class HellobootApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello Container less Standalone Application");
+    }
+
+}
