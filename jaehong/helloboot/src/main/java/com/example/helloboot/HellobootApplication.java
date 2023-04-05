b4988c148a43ee0df37fb08ed4157101e6ec887b
JaehongDev <println@kakao.com>
2023-04-05 23:42:19 +0900

    feature: apply servlet

--- jaehong/helloboot/src/main/java/com/example/helloboot/HellobootApplication.java
+++ jaehong/helloboot/src/main/java/com/example/helloboot/HellobootApplication.java
@@ -1,9 +1,27 @@
 package com.example.helloboot;
 
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+
 public class HellobootApplication {
     public static void main(String[] args) {
-        System.out.println("Hello Container less Standalone Application");
+        final var tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        final var webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                    response.setStatus(200);
+                    response.setHeader("Content-Type", "text/plain");
+                    response.getWriter().println("Hello Servlet");
+                }
+            }).addMapping("/hello");
+        });
+        webServer.start();
     }
 
 }
