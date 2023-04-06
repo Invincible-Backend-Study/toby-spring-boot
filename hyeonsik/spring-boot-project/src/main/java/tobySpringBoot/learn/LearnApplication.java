008181cf30df1a2d02be41b7b34b696f4e39a70d
Basak <chgy2131@naver.com>
2023-04-06 15:54:41 +0900

    feat(CH03 - 서블릿 등록): SpringBoot의 내장 톰캣 실행 구문을 제거하고 직접 Servlet을 매칭하도록 수정

--- hyeonsik/spring-boot-project/src/main/java/tobySpringBoot/learn/LearnApplication.java
+++ hyeonsik/spring-boot-project/src/main/java/tobySpringBoot/learn/LearnApplication.java
@@ -2,12 +2,34 @@ package tobySpringBoot.learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class LearnApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnApplication.class, args);
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();  // Factory에 기본적인 설정들이 잡혀있다.
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					// response 를 만든다.
+					resp.setStatus(200);
+					resp.setHeader("Content-Type", "text/plain");
+					resp.getWriter().print("Hello Servlet");
+				}
+			}).addMapping("/hello");  // path pattern matching
+		});
+		webServer.start();
 	}
 
 }
