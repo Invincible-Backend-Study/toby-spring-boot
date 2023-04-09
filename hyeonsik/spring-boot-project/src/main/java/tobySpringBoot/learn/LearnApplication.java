de10d11b2e6b33f9044e0dae389b83b5d0f32eb5
Basak <chgy2131@naver.com>
2023-04-10 00:52:51 +0900

    feat(CH04): DispatcherServlet 전환 및 mapping 정보 추가 중

--- hyeonsik/spring-boot-project/src/main/java/tobySpringBoot/learn/LearnApplication.java
+++ hyeonsik/spring-boot-project/src/main/java/tobySpringBoot/learn/LearnApplication.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -23,34 +25,16 @@ public class LearnApplication {
 
 	public static void main(String[] args) {
 		// SpringContainer를 만든다.
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		applicationContext.registerBean(HelloController.class);  // 생성할 빈들을 명시
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();  // 이걸 통해 빈 오브젝트들을 만들어준다.
 
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();  // Factory에 기본적인 설정들이 잡혀있다.
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					// 인증, 보안, 다국어, 공통 기능 등에 대한 처리가 들어가야함
-					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-						// 파라미터 분석
-						String name = req.getParameter("name");
-
-						HelloController helloController = applicationContext.getBean(HelloController.class);  // 등록된 빈을 가져온다.
-						String ret = helloController.hello(name);
-
-						// response 를 만든다.
-						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().print(ret);
-					}
-					else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-				}
-			}).addMapping("/*");  // path pattern matching
+			servletContext.addServlet("dispatcherServlet",
+							new DispatcherServlet(applicationContext)
+					).addMapping("/*");  // path pattern matching
 		});
 		webServer.start();
 	}
