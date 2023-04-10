49dd3c99b7c4351fc137046b17a7c1794c2be9e2
wnsgh12s <54989191+wnsgh12s@users.noreply.github.com>
2023-04-11 06:04:32 +0900

    feat: 스프링 컨테이너 통합

--- junho/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ junho/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -23,17 +23,21 @@ public class HellobootApplication {
 
 	public static void main(String[] args) {
 
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+				TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet",new DispatcherServlet(this))
+							.addMapping("/*");
+				});
+				webServer.start();
+			}
+		};
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
-
-		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("dispatcherServlet",new DispatcherServlet(applicationContext))
-					.addMapping("/*");
-		});
-		webServer.start();
 	}
 
 }
