8a559547fdeb67e0a1545537d9dfb79796589664
HiiWee <wpdlzhf159@gmail.com>
2023-04-10 21:50:35 +0900

    remove: @SpringBootApplication과 초기 스프링 부트 run 코드 삭제

--- hoseok/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ hoseok/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,9 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+		System.out.println("Hello Containerless Standalone Application");
 	}
 
 }
