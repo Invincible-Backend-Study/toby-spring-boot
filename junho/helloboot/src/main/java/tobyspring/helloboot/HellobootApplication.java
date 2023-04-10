8c7db83ae920d80c4726a614c64a084743a0724b
wnsgh12s <54989191+wnsgh12s@users.noreply.github.com>
2023-04-11 05:06:52 +0900

    feat: 스프링 컨테이너 사용

--- junho/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ junho/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 
@@ -16,23 +17,32 @@ import java.io.IOException;
 @SpringBootApplication
 public class HellobootApplication {
 
+
 	public static void main(String[] args) {
+
+		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		applicationContext.registerBean(HelloController.class);
+		applicationContext.refresh();
+
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
 			servletContext.addServlet("hello", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp)
 						throws ServletException, IOException {
+
 					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 						String name = req.getParameter("name");
 
-						String rt = new HelloController().hello(name);
+						HelloController helloController = applicationContext.getBean(HelloController.class);
+						String rt = helloController.hello(name);
 
 						resp.setCharacterEncoding("UTF-8");
 						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
 						resp.getWriter().println(rt);
 						return;
 					}
+
 				}
 			}).addMapping("/");
 		});
