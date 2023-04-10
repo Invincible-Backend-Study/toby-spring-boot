e464e29076127cebf083d832a98e89526a3f78d9
HiiWee <wpdlzhf159@gmail.com>
2023-04-11 03:51:01 +0900

    feat: 어노테이션 매핑 정보를 사용해 dispatcherServlet이 정상적으로 매핑을 찾을 수 있게 함

--- hoseok/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ hoseok/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,7 +3,11 @@ package tobyspring.helloboot;
 import java.util.Objects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping
 public class HelloController {
 
     private static final Logger log = LoggerFactory.getLogger(HelloController.class);
@@ -14,6 +18,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(final String name) {
         log.info("call HelloController#hello");
         return helloService.sayHello(Objects.requireNonNull(name));
