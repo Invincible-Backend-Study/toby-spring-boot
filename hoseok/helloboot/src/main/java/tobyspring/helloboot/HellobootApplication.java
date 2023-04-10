d021b1d2328439472e9b67c8316c0c03c0500899
HiiWee <wpdlzhf159@gmail.com>
2023-04-10 23:51:11 +0900

    feat: 서블릿 컨텍스트에 서블릿 등록 및 매핑

--- hoseok/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ hoseok/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,17 +1,32 @@
 package tobyspring.helloboot;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 public class HellobootApplication {
 
-	public static void main(String[] args) {
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+    public static void main(String[] args) {
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
-		// 톰캣외에 제티, 언더토우 같은 유명한 서블릿 컨테이너를 지원할 수 있고, 일관된 방식으로 동작하도록 추상화했기에 Tomcat이 보이지 않음
-		WebServer webServer = serverFactory.getWebServer();
-		webServer.start();
-	}
+        // 톰캣외에 제티, 언더토우 같은 유명한 서블릿 컨테이너를 지원할 수 있고, 일관된 방식으로 동작하도록 추상화했기에 Tomcat이 보이지 않음
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(final HttpServletRequest req, final HttpServletResponse resp)
+						throws ServletException, IOException {
+                    resp.setStatus(200);
+                    resp.setHeader("Content-Type", "text/plain");
+                    resp.getWriter().print("hello");
+                }
+            }).addMapping("/hello");
+        });
+        webServer.start();
+    }
 
 }
