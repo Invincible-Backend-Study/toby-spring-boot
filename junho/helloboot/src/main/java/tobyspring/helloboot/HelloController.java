7c81d10c4b5959f67a3dd98c8d1b301df325ff02
wnsgh12s <54989191+wnsgh12s@users.noreply.github.com>
2023-04-11 05:00:54 +0900

    feat: 프론트 컨트롤러 추가

--- junho/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ junho/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,10 +3,11 @@ package tobyspring.helloboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class HelloController {
-    @GetMapping("/hello")
     public String hello(String name) {
+        if(name == null){
+            name = "";
+        }
         return "안녕" + name;
     }
 }
--- junho/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ junho/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,12 +2,41 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @SpringBootApplication
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp)
+						throws ServletException, IOException {
+					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						String name = req.getParameter("name");
+
+						String rt = new HelloController().hello(name);
+
+						resp.setCharacterEncoding("UTF-8");
+						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println(rt);
+						return;
+					}
+				}
+			}).addMapping("/");
+		});
+		webServer.start();
 	}
 
 }
