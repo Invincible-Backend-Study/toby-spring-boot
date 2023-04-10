ccdefb08c35844d1aaa4ec62797a16b5cf846098
HiiWee <wpdlzhf159@gmail.com>
2023-04-11 02:08:04 +0900

    feat: 스프링 컨테이너를 사용해 프론트 컨트롤러 패턴 적용

--- hoseok/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ hoseok/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,10 +16,16 @@ import org.springframework.http.MediaType;
 public class HellobootApplication {
 
     public static void main(String[] args) {
+        // ApplicationContext는 애플리케이션을 구성하는 많은 정보, 리소스에 접근하는 방법, 내부 이벤트 전달, 구독방법 등 많은 작업들을 수행하는 기능들을 담고있다.
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        // HelloController를 bean으로 등록
+        applicationContext.registerBean(HelloController.class);
+        // 자기가 가지고 있는 정보를 활용해 컨텍스트를 초기화
+        applicationContext.refresh();
+
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         // 톰캣외에 제티, 언더토우 같은 유명한 서블릿 컨테이너를 지원할 수 있고, 일관된 방식으로 동작하도록 추상화했기에 Tomcat이 보이지 않음
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            HelloController helloController = new HelloController();
 
             servletContext.addServlet("frontController", new HttpServlet() {
                 @Override
@@ -29,14 +35,11 @@ public class HellobootApplication {
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                         String name = req.getParameter("name");
 
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
                         String ret = helloController.hello(name);
 
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                         resp.getWriter().print(ret);
-                        return;
-                    }
-                    if (req.getRequestURI().equals("/user")) {
                         return;
                     }
                     resp.setStatus(HttpStatus.NOT_FOUND.value());
