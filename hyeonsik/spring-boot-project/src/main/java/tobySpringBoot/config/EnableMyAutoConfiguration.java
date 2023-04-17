39aa42a679117e2b41c7cfd90ebea0ae24c79494
Basak <chgy2131@naver.com>
2023-04-17 23:12:05 +0900

    feat(CH06): Import 구문이 최상위 애노테이션에서 직접 노출되지 않도록 분리 - EnableMyAutoConfiguration 애노테이션 추가 및 적용

--- hyeonsik/spring-boot-project/src/main/java/tobySpringBoot/config/EnableMyAutoConfiguration.java
+++ hyeonsik/spring-boot-project/src/main/java/tobySpringBoot/config/EnableMyAutoConfiguration.java
@@ -1,9 +1,6 @@
-package tobySpringBoot.learn;
+package tobySpringBoot.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import tobySpringBoot.config.EnableMyAutoConfiguration;
 import tobySpringBoot.config.autoconfig.DispatcherServletConfig;
 import tobySpringBoot.config.autoconfig.TomcatWebServerConfig;
 
@@ -14,8 +11,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Configuration
-@ComponentScan
-@EnableMyAutoConfiguration
-public @interface MySpringBootApplication {
+@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})
+public @interface EnableMyAutoConfiguration {
 }
