f88566006645fe888af15852690b10b0cc20a16b
HiiWee <wpdlzhf159@gmail.com>
2023-04-18 04:07:39 +0900

    feat: @Configuration과 proxyBeanMethods

---
+++ hoseok/helloboot/src/test/java/tobyspring/study/ConfigurationTest.java
@@ -0,0 +1,82 @@
+package tobyspring.study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class ConfigurationTest {
+
+    @Test
+    void configuration() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
+        ac.register(MyConfig.class);
+        ac.refresh();
+
+        Bean1 bean1 = ac.getBean(Bean1.class);
+        Bean2 bean2 = ac.getBean(Bean2.class);
+
+        assertThat(bean1.common).isSameAs(bean2.common);
+    }
+
+    @Test
+    void proxyCommonMethod() {
+        MyConfigProxy myConfigProxy = new MyConfigProxy();
+        Bean1 bean1 = myConfigProxy.bean1();
+        Bean2 bean2 = myConfigProxy.bean2();
+
+        assertThat(bean1.common).isSameAs(bean2.common);
+    }
+
+    static class MyConfigProxy extends MyConfig {
+        private Common common;
+
+        @Override
+        Common common() {
+            if (this.common == null) {
+                this.common = super.common();
+            }
+            return this.common;
+        }
+    }
+
+    @Configuration
+    static class MyConfig {
+        @Bean
+        Common common() {
+            return new Common();
+        }
+
+        @Bean
+        Bean1 bean1() {
+            return new Bean1(common());
+        }
+
+        @Bean
+        Bean2 bean2() {
+            return new Bean2(common());
+        }
+    }
+
+    static class Bean1 {
+        private final Common common;
+
+        public Bean1(final Common common) {
+            this.common = common;
+        }
+    }
+
+    static class Bean2 {
+        private final Common common;
+
+        public Bean2(final Common common) {
+            this.common = common;
+        }
+    }
+
+    static class Common {
+
+    }
+}
