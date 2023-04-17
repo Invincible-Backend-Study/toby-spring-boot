3cfb82801412c43503c26fa944d9cdcfa9c195e6
HiiWee <wpdlzhf159@gmail.com>
2023-04-17 20:09:23 +0900

    feat: 테스트 코드를 이용한 테스트

---
+++ hoseok/helloboot/src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -0,0 +1,35 @@
+package tobyspring.helloboot;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+public class HelloApiTest {
+
+    @Test
+    void helloApi() {
+        // http localhost:8080/hello?name=spring
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> spring = rest.getForEntity("http://localhost:8080/hello?name={name}", String.class,
+                "Spring");
+
+        // 응답 검증 3가지
+        Assertions.assertAll(
+                // status code 200
+                () -> assertThat(spring.getStatusCode()).isEqualTo(HttpStatus.OK),
+                // header(Content-Type) text/plain
+                () -> assertThat(spring.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(
+                        MediaType.TEXT_PLAIN_VALUE),
+                // body Hello Spring
+                () -> assertThat(spring.getBody()).isEqualTo("Hello Spring")
+        );
+    }
+
+}
