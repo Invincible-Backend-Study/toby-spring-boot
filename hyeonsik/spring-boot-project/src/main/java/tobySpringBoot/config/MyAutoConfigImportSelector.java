ee967680b8f4ad18a2b464b0849c26d6c1450941
Basak <chgy2131@naver.com>
2023-04-18 00:45:10 +0900

    feat(CH06): Import할 패키지정보를 외부에 명시

--- hyeonsik/spring-boot-project/src/main/java/tobySpringBoot/config/MyAutoConfigImportSelector.java
+++ hyeonsik/spring-boot-project/src/main/java/tobySpringBoot/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,25 @@
 package tobySpringBoot.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "tobySpringBoot.config.autoconfig.DispatcherServletConfig",
-                "tobySpringBoot.config.autoconfig.TomcatWebServerConfig"
-        };
+        List<String> autoConfigs = new ArrayList<>();
+
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+        return autoConfigs.toArray(new String[0]);
     }
 }
--- hyeonsik/spring-boot-project/src/main/java/tobySpringBoot/config/autoconfig/DispatcherServletConfig.java
+++ hyeonsik/spring-boot-project/src/main/java/tobySpringBoot/config/autoconfig/DispatcherServletConfig.java
@@ -3,8 +3,9 @@ package tobySpringBoot.config.autoconfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
+import tobySpringBoot.config.MyAutoConfiguration;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
         @Bean
         public DispatcherServlet dispatcherServlet() {
