aaae1bce49169bd9e5d1607516a9bc57edd4d65d
Basak <chgy2131@naver.com>
2023-04-18 00:45:24 +0900

    feat(CH06): configuration proxy test

---
+++ hyeonsik/spring-boot-project/src/test/java/tobySpringBoot/study/ConfigurationTest.java
@@ -0,0 +1,71 @@
+package tobySpringBoot.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConfigurationTest {
+    @Test
+    void configuration() {
+    }
+
+    @Test
+    void proxyCommonMethod() {
+        MyConfigProxy myConfigProxy = new MyConfigProxy();
+
+        Bean1 bean1 = myConfigProxy.bean1();
+        Bean2 bean2 = myConfigProxy.bean2();
+
+        assertThat(bean1.common).isSameAs(bean2.common);
+    }
+
+    // 아래와 같은 느낌으로 proxy 가 생성된다.
+    static class MyConfigProxy extends MyConfig {
+        private Common common;
+
+        @Override
+        Common common() {
+            if (this.common == null) this.common = super.common();
+            return this.common;
+        }
+    }
+
+    @Configuration
+    static class MyConfig {
+        @Bean
+        Common common() {
+            return new Common();
+        }
+
+        @Bean
+        Bean1 bean1() {
+            return new Bean1(common());
+        }
+
+        @Bean
+        Bean2 bean2() {
+            return new Bean2(common());
+        }
+    }
+
+    static class Bean1 {
+        private final Common common;
+
+        Bean1(Common common) {
+            this.common = common;
+        }
+    }
+
+    static class Bean2 {
+        private final Common common;
+
+        Bean2(Common common) {
+            this.common = common;
+        }
+    }
+
+    static class Common {
+    }
+}
