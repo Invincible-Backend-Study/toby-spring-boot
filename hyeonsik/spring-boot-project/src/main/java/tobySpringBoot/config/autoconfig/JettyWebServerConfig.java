bacff8777074bf79da3d7d384112403675210adf
Basak <chgy2131@naver.com>
2023-04-24 22:23:59 +0900

    feat(CH07): 자동 구성 정보 개발자가 대체하기

--- hyeonsik/spring-boot-project/src/main/java/tobySpringBoot/config/autoconfig/JettyWebServerConfig.java
+++ hyeonsik/spring-boot-project/src/main/java/tobySpringBoot/config/autoconfig/JettyWebServerConfig.java
@@ -1,5 +1,6 @@
 package tobySpringBoot.config.autoconfig;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -8,20 +9,16 @@ import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.util.ClassUtils;
+import tobySpringBoot.config.ConditionalMyOnClass;
 import tobySpringBoot.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-@Conditional(JettyWebServerConfig.JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     @Bean("jettyWebServerFactory")  // bean name은 기본적으로 method 명을 따라간다. 동일 이름이 있으면 충돌이 날 수 있으니 이름을 구분!!
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
     }
 
-    static class JettyCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return ClassUtils.isPresent("org.eclipse.jetty.server.Server", context.getClassLoader());
-        }
-    }
 }
