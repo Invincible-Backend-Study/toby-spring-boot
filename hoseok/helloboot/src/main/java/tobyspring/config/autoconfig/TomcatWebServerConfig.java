7df9bb255f3a88f884e527628a4facca0968e786
HiiWee <wpdlzhf159@gmail.com>
2023-04-25 02:38:03 +0900

    feat: 자동 구성에 Environment 프로퍼티 적용

--- hoseok/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ hoseok/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
+import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.util.ClassUtils;
 import tobyspring.config.ConditionalMyOnClass;
@@ -16,10 +17,13 @@ import tobyspring.config.MyAutoConfiguration;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat") // 여길 통과하면
 public class TomcatWebServerConfig {
 
+
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean  // 요런 타입의 빈이 있다면(유저 구성 정보) 등록하지 않고, 없다면 등록한다.
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.setContextPath(env.getProperty("contextPath"));
+        return factory;
     }
 
 }
