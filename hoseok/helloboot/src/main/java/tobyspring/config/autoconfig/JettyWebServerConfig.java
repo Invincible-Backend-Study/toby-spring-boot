bf4da0516ffc50dea72b005bc8b60334b2e551b8
HiiWee <wpdlzhf159@gmail.com>
2023-04-24 15:42:45 +0900

    feat: 스타터와 Jetty 서버 구성 추가

--- hoseok/helloboot/src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
+++ hoseok/helloboot/src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
@@ -1,6 +1,6 @@
 package tobyspring.config.autoconfig;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Condition;
@@ -9,22 +9,23 @@ import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import tobyspring.config.MyAutoConfiguration;
 
-@MyAutoConfiguration
-@Conditional(TomcatWebServerConfig.TomcatCondition.class)
-public class TomcatWebServerConfig {
 
-    @Bean("tomcatWebServerFactory")
+@MyAutoConfiguration
+@Conditional(JettyWebServerConfig.JettyCondition.class)
+public class JettyWebServerConfig {
+
+    @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 
-    static class TomcatCondition implements Condition {
+    static class JettyCondition implements Condition {
 
         @Override
         public boolean matches(final ConditionContext context, final AnnotatedTypeMetadata metadata) {
-            return false;
+
+            return true;
         }
 
     }
-
 }
