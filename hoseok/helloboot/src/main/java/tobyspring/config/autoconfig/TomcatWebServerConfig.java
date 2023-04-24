8529bb9e132ca849d4a8bac22e6dfab9b8e4efd5
HiiWee <wpdlzhf159@gmail.com>
2023-04-25 03:00:13 +0900

    feat: @Value와 PropertySourcesPlaceholderConfigurer

--- hoseok/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ hoseok/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,5 +1,6 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -17,12 +18,15 @@ import tobyspring.config.MyAutoConfiguration;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat") // 여길 통과하면
 public class TomcatWebServerConfig {
 
+    @Value("${contextPath}") // BeanFactoryPostProcessor 라는 스프링 컨테이너의 후처리기를 빈으로 등록하면 정상적으로 치환할 수 있다.
+    String contextPath;
 
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean  // 요런 타입의 빈이 있다면(유저 구성 정보) 등록하지 않고, 없다면 등록한다.
-    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+    public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath"));
+        System.out.println(contextPath);
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 
